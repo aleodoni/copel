dd19aba5f17d0faae39cba9c5dc125b5b7f6063f
aleodoni <aleodoni@gmail.com>
2021-10-14 14:59:21 -0300

    feat: Add router paths

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Flex, Text, Button, HStack, Box } from '@chakra-ui/react'
 import { format } from 'date-fns'
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export const Header = () => {
@@ -8,12 +9,18 @@ export const Header = () => {
   const [number, setNumber] = useState('')
   const [error, setError] = useState(null)
 
+  const router = useRouter()
+
   function handleClick(value: string): void {
     setNumber(number + value)
   }
 
   function handleClean(): void {
     setNumber('');
+  }
+
+  function handleVoltar(): void {
+    router.back();
   }
 
   async function handleSubmit(): Promise<void> {
@@ -28,6 +35,7 @@ export const Header = () => {
     if(resp.status === 200) {
       setError(null)
       setNumber('')
+      router.push('/')
     } else {
       setError('Erro ao inserir leitura')
     }
@@ -48,26 +56,27 @@ export const Header = () => {
         </Box>
       </HStack>
       <HStack spacing={4} mt={4} w="full" justifyContent="space-between">
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('1')}>1</Button>
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('2')}>2</Button>
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('3')}>3</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('1')}>1</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('2')}>2</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('3')}>3</Button>
       </HStack>
       <HStack spacing={4} mt={4} w="full" justifyContent="space-between">
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('4')}>4</Button>
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('5')}>5</Button>
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('6')}>6</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('4')}>4</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('5')}>5</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('6')}>6</Button>
       </HStack>
       <HStack spacing={4} mt={4} w="full" justifyContent="space-between">
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('7')}>7</Button>
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('8')}>8</Button>
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('9')}>9</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('7')}>7</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('8')}>8</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('9')}>9</Button>
       </HStack>
       <HStack spacing={4} mt={4} w="full" justifyContent="space-between">
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClick('0')}>0</Button>
-        <Button bg='gray.800' color='white' size="lg" onClick={() => handleClean() }>C</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClick('0')}>0</Button>
+        <Button colorScheme="orange" size="lg" onClick={() => handleClean() }>C</Button>
       </HStack>
       <HStack spacing={4} mt={4} w="full" justifyContent="center">
-        <Button bg='gray.800' color='white' size="lg" w="full" onClick={() => handleSubmit()}>Salvar</Button>
+        <Button colorScheme="orange" size="lg" w="full" onClick={() => handleSubmit()}>Salvar</Button>
+        <Button size="lg" colorScheme="orange" w="full" onClick={() => handleVoltar()}>Voltar</Button>
       </HStack>
       { error &&  
         <Text fontSize="xl" color="red.600" p={4}>{ error }</Text>        
